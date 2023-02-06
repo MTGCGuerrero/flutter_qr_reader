4c7d754c299f6778f015759a42881984f21d51d2
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-02-06 17:37:08 -0300

    add provider

--- lib/widgets/custom_navigatorbar.dart
+++ lib/widgets/custom_navigatorbar.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
-class CustomNavigationBar extends StatelessWidget {
 
+class CustomNavigationBar extends StatelessWidget {
+  final currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      currentIndex: 0,
+      currentIndex: currentIndex,
       elevation: 0,
       items: [
-      BottomNavigationBarItem(icon: Icon(Icons.map),label: 'Mapa'),
-      BottomNavigationBarItem(icon: Icon(Icons.compass_calibration), label: 'Direcciones'),
-    ],);
+        BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Mapa'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.compass_calibration), label: 'Direcciones'),
+      ],
+    );
   }
 }
